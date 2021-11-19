4b0a317e1917afda3dfec0e8f03d3dfa23bff81a
MohamedDib <49766520+MohamedDib@users.noreply.github.com>
2021-11-19 13:20:03 +0100

    Init Client service : get current weather of city

---
+++ client-get-current-weather/src/main/java/com/m2dfs/clientgetcurrentweather/ClientGetCurrentWeatherApplication.java
@@ -0,0 +1,13 @@
+package com.m2dfs.clientgetcurrentweather;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClientGetCurrentWeatherApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClientGetCurrentWeatherApplication.class, args);
+	}
+
+}
