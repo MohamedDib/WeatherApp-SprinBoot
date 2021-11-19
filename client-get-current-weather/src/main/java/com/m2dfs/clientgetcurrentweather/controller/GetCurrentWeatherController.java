cbc0578aabc684e545a3f8371311cd04612a748f
MohamedDib <49766520+MohamedDib@users.noreply.github.com>
2021-11-19 19:02:53 +0100

    Adding Swagger to Client Weather service

--- client-get-current-weather/src/main/java/com/m2dfs/clientgetcurrentweather/controller/GetCurrentWeatherController.java
+++ client-get-current-weather/src/main/java/com/m2dfs/clientgetcurrentweather/controller/GetCurrentWeatherController.java
@@ -1,7 +1,10 @@
 package com.m2dfs.clientgetcurrentweather.controller;
 
 import com.google.gson.Gson;
+import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.core.ParameterizedTypeReference;
@@ -14,12 +17,18 @@ import org.springframework.web.client.RestTemplate;
 
 import java.util.Map;
 
+@Api(value = "GetCurrentWeatherController", description = "Here we get the current weather data from other services endpoints by using the city name")
 @RestController
 public class GetCurrentWeatherController {
 
     @Autowired
     RestTemplate restTemplate;
 
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "Suceess|OK"),
+            @ApiResponse(code = 401, message = "not authorized!"),
+            @ApiResponse(code = 403, message = "forbidden!!!"),
+            @ApiResponse(code = 404, message = "not found!!!") })
     @RequestMapping(value = "/getCurrentWeatherOfCity/{cityName}", method = RequestMethod.GET)
     public String getWeather(@PathVariable String cityName) {
 
