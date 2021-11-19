b22ed267fd416ddb6c52fd22f6e0da013ada5009
MohamedDib <49766520+MohamedDib@users.noreply.github.com>
2021-11-19 17:42:00 +0100

    Circuit Breaker

--- client-get-current-weather/src/main/java/com/m2dfs/clientgetcurrentweather/ClientGetCurrentWeatherApplication.java
+++ client-get-current-weather/src/main/java/com/m2dfs/clientgetcurrentweather/ClientGetCurrentWeatherApplication.java
@@ -2,8 +2,12 @@ package com.m2dfs.clientgetcurrentweather;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 
 @SpringBootApplication
+@EnableCircuitBreaker
+
 public class ClientGetCurrentWeatherApplication {
 
 	public static void main(String[] args) {
