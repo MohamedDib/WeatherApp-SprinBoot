3e79997b47e74c23094cb75a30bc13e421b888b5
MohamedDib <49766520+MohamedDib@users.noreply.github.com>
2021-11-19 18:39:56 +0100

    Adding Eureka clients

--- city-service/src/test/java/com/m2dfs/cityservice/CityServiceApplicationTests.java
+++ city-service/src/test/java/com/m2dfs/cityservice/CityServiceApplicationTests.java
@@ -2,8 +2,10 @@ package com.m2dfs.cityservice;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootTest
+@EnableEurekaClient
 class CityServiceApplicationTests {
 
 	@Test
--- client-get-current-weather/src/main/java/com/m2dfs/clientgetcurrentweather/ClientGetCurrentWeatherApplication.java
+++ client-get-current-weather/src/main/java/com/m2dfs/clientgetcurrentweather/ClientGetCurrentWeatherApplication.java
@@ -2,8 +2,10 @@ package com.m2dfs.clientgetcurrentweather;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class ClientGetCurrentWeatherApplication {
 
 	public static void main(String[] args) {
