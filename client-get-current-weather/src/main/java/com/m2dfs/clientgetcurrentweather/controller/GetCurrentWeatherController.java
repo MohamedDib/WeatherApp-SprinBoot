be894d9520c4782f9b5e2a67669ab40eec4d8cde
MohamedDib <49766520+MohamedDib@users.noreply.github.com>
2021-11-19 17:43:14 +0100

    One step back : remove circuit breaker , error took more thank  hours

--- client-get-current-weather/src/main/java/com/m2dfs/clientgetcurrentweather/controller/GetCurrentWeatherController.java
+++ client-get-current-weather/src/main/java/com/m2dfs/clientgetcurrentweather/controller/GetCurrentWeatherController.java
@@ -1,25 +1,59 @@
 package com.m2dfs.clientgetcurrentweather.controller;
 
-import com.m2dfs.clientgetcurrentweather.delegate.CurrentWeatherServiceDelegate;
+import com.google.gson.Gson;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.HttpMethod;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.Map;
 
 @RestController
 public class GetCurrentWeatherController {
 
     @Autowired
-    CurrentWeatherServiceDelegate currentWeatherServiceDelegate;
+    RestTemplate restTemplate;
 
     @RequestMapping(value = "/getCurrentWeatherOfCity/{cityName}", method = RequestMethod.GET)
     public String getWeather(@PathVariable String cityName) {
 
-        System.out.println("Calling current weather service to get Data ...");
-        //return currentWeatherServiceDelegate.callCurrentWeatherServiceAndGetData(cityName);
-        return currentWeatherServiceDelegate.callCurrentWeatherServiceAndGetData(cityName);
+        String cityCode = getCityCode(cityName);
+
+        String response = restTemplate.exchange(
+                "http://localhost:8181/currentWeather/"+cityCode,
+                HttpMethod.GET,
+                null,
+                new ParameterizedTypeReference<String>() {},
+                cityName).getBody();
+        return response;
     }
 
+    public String getCityCode(String cityName) {
+        String response = restTemplate.exchange(
+                "http://localhost:8282/citydetails/{cityName}",
+                HttpMethod.GET,
+                null,
+                new ParameterizedTypeReference<String>() {},
+                cityName).getBody();
+
+        // parse JSON and get only the code from city Infos
+        String jsonRes = response;
+        jsonRes = jsonRes.substring(1, jsonRes.length() - 1);
+        Map jsonObj = new Gson().fromJson(jsonRes, Map.class);
+        String cityCode = (String) jsonObj.get("Key");
+
+        return cityCode;
+    }
+
+    @Bean
+    RestTemplate restTemplate() {
+        return new RestTemplate();
+    }
 }
 
