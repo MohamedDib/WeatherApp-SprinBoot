e5c8096b2f65aef858ca101743beb50ee4a254de
MohamedDib <49766520+MohamedDib@users.noreply.github.com>
2021-11-19 18:57:50 +0100

    Adding Swagger to Weather Service

--- weather-service/src/main/java/com/m2dfs/weatherservice/controller/CurrentWeatherServiceController.java
+++ weather-service/src/main/java/com/m2dfs/weatherservice/controller/CurrentWeatherServiceController.java
@@ -1,6 +1,9 @@
 package com.m2dfs.weatherservice.controller;
 
+import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.core.ParameterizedTypeReference;
@@ -11,6 +14,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
+@Api(value = "CurrentWeatherServiceController", description = "Getting current weather info from AccuWeather of specefic city")
 @RestController
 public class CurrentWeatherServiceController {
 
@@ -21,6 +25,11 @@ public class CurrentWeatherServiceController {
     private String language = "fr-fr";
 
     @ApiOperation(value = "Get today's weather", response = CurrentWeatherServiceController.class, tags = "getWeather")
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "Suceess|OK"),
+            @ApiResponse(code = 401, message = "not authorized!"),
+            @ApiResponse(code = 403, message = "forbidden!!!"),
+            @ApiResponse(code = 404, message = "not found!!!") })
     @RequestMapping(value = "/currentWeather/{key}", method = RequestMethod.GET)
     public String getWeather(@PathVariable String key) {
 
